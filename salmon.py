6242ae30df88757cd7ad7eddcafed4b919680930
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2022-10-04 09:56:13 +0300

    Add files via upload

---
+++ salmon.py
@@ -0,0 +1,7 @@
+from project_exam.food.main_dish import MainDish
+
+class Salmon(MainDish):
+    GRAMS = 22
+
+    def __init__(self, name, price):
+        super().__init__(name, price, self.GRAMS)
