b5bb10b0b53d815a9c42f2b0f6ae5e88adf2321e
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2022-10-06 08:51:33 +0300

    Add files via upload

---
+++ beverage.py
@@ -0,0 +1,10 @@
+from project_exam.product import Product
+
+class Beverage(Product):
+    def __init__(self, name, price, milliliters):
+        super().__init__(name, price)
+        self.__milliliters = milliliters
+
+    @property
+    def milliliters(self):
+        return self.__milliliters
